378be4bbb1a8b8c297c5a4e49b097a948f740220
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-01 17:14:04 -0300

    feat: error handler middleware

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,31 @@
+import { AppError } from '@/utils/AppError'
+import { Request, Response, NextFunction } from 'express'
+import { ZodError } from 'zod'
+
+export const errorHandler = (
+    error: Error,
+    request: Request,
+    response: Response,
+    _: NextFunction,
+) => {
+    if (error instanceof ZodError) {
+        const formattedIssues = error.errors.map((issue) => ({
+            field: issue.path[0],
+            message: issue.message,
+            code: issue.code,
+        }))
+
+        return response.status(400).json({
+            message: 'Validation error:',
+            issues: formattedIssues,
+        })
+    }
+
+    if (error instanceof AppError) {
+        return response
+            .status(error.statusCode)
+            .json({ message: error.message })
+    }
+
+    return response.status(500).json({ message: error.message })
+}
