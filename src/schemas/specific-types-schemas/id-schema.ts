b0369a4f3a48df808b4b3d388ea7172da59168eb
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-02 18:04:50 -0300

    feat: create id schema validation

---
+++ src/schemas/specific-types-schemas/id-schema.ts
@@ -0,0 +1,25 @@
+import {
+    invalidTypeErrorMessage,
+    requiredErrorMessage,
+} from '@/utils/validation-errors'
+import z from 'zod'
+
+export const idBodySchema = z.number({
+    invalid_type_error: invalidTypeErrorMessage('id', 'number'),
+    required_error: requiredErrorMessage('id'),
+})
+
+export const idParamSchema = z
+    .string()
+    .transform((value) => Number(value))
+    .refine((value) => !isNaN(value), {
+        message: invalidTypeErrorMessage('id', 'number'),
+    })
+
+export const idQueryParam = z
+    .string()
+    .transform((value) => Number(value))
+    .refine((value) => !isNaN, {
+        message: invalidTypeErrorMessage('id', 'number'),
+    })
+    .optional()
