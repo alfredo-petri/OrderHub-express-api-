a205906e4c91174088ad3400a7aab7b257429109
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-02 18:06:00 -0300

    feat: create password schema validation

---
+++ src/schemas/specific-types-schemas/password-schema.ts
@@ -0,0 +1,21 @@
+import {
+    invalidTypeErrorMessage,
+    minStringErrorMessage,
+    passwordEspecialCharErrorMessage,
+    passwordNumberErrorMessage,
+    passwordUppercaseErrorMessage,
+    requiredErrorMessage,
+} from '@/utils/validation-errors'
+import z from 'zod'
+
+export const passwordSchema = () => {
+    return z
+        .string({
+            invalid_type_error: invalidTypeErrorMessage('password', 'string'),
+            required_error: requiredErrorMessage('password'),
+        })
+        .min(8, minStringErrorMessage('password', 8))
+        .regex(/[0-9]/, passwordNumberErrorMessage)
+        .regex(/[\W_]/, passwordEspecialCharErrorMessage)
+        .regex(/[A-Z]/, passwordUppercaseErrorMessage)
+}
