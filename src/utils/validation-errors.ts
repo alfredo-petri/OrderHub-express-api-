762bf2026d8aba8e8347d609c01be18259435269
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-02 18:09:00 -0300

    feat: update error messages for validation

--- src/utils/validation-errors.ts
+++ src/utils/validation-errors.ts
@@ -20,5 +20,18 @@ export const greaterThanErrorMessage = (
     field: string,
     minValue: number,
 ): string => {
-    return `${field} must be greater than ${minValue}`
+    return `${field} value must be greater than ${minValue}`
 }
+
+export const invalidEmailErrorMessage = `email value must be a valid email`
+
+// password errors
+
+export const passwordNumberErrorMessage =
+    'password must contain at least one number'
+
+export const passwordEspecialCharErrorMessage =
+    'password must contain at least one special character (!@#$%&*?)'
+
+export const passwordUppercaseErrorMessage =
+    'password must contain at least one uppercase letter'
