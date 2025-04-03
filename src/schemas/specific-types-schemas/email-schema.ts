7cc36fec74c2a5d57a47267b1ef9a526deee8f86
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-03 15:18:49 -0300

    perf: improve email schema validation

--- src/schemas/specific-types-schemas/email-schema.ts
+++ src/schemas/specific-types-schemas/email-schema.ts
@@ -1,11 +1,15 @@
 import {
     invalidEmailErrorMessage,
+    invalidTypeErrorMessage,
     requiredErrorMessage,
 } from '@/utils/validation-errors'
 import z from 'zod'
 
 export const emailSchema = (field: string) => {
     return z
-        .string({ required_error: requiredErrorMessage(field) })
+        .string({
+            required_error: requiredErrorMessage(field),
+            invalid_type_error: invalidTypeErrorMessage('email', 'string'),
+        })
         .email({ message: invalidEmailErrorMessage })
 }
