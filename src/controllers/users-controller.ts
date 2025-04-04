97e6847db90e35d0bb33d56d784dba0403e0e5ea
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-04 13:40:22 -0300

    feat: add create user method to user controler

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,15 @@
+import { User } from '@/class/user'
+import { createUserSchema } from '@/schemas/users/create-user-schema'
 import { Request, Response, NextFunction } from 'express'
 
 class UsersController {
-    create(request: Request, response: Response, next: NextFunction) {
+    async create(request: Request, response: Response, next: NextFunction) {
         try {
-            return response.json({ message: 'ok' })
+            const data = createUserSchema.parse(request.body)
+
+            const newUser = await User.new(data)
+
+            return response.status(201).json({ newUser })
         } catch (error) {
             next(error)
         }
