8e82d6d9d3a9f622b96c8134ac164605951938b7
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-04 14:27:21 -0300

    feat: add erro message on queries

--- src/utils/db-queries-errors.ts
+++ src/utils/db-queries-errors.ts
@@ -1,2 +1,3 @@
 export const userNotFound = 'No user found with the provided identifier'
 export const emailInUse = 'email is already in use'
+export const credentialsError = 'email or password invalid'
