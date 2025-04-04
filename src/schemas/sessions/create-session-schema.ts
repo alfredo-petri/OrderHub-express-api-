3bc983c23501de9b907e43cc87f87f750e8153d5
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-04 14:40:31 -0300

    fix: bug on create session schema

--- src/schemas/sessions/create-session-schema.ts
+++ src/schemas/sessions/create-session-schema.ts
@@ -1,10 +1,10 @@
 import z from 'zod'
 import { emailSchema } from '../specific-types-schemas/email-schema'
-import { passwordSchema } from '../specific-types-schemas/password-schema'
+import { stringSchema } from '../type-schemas/string-schemas'
 
 const createSessionSchema = z.object({
     email: emailSchema('email'),
-    password: passwordSchema(),
+    password: stringSchema('password', 'string'),
 })
 
 export { createSessionSchema }
