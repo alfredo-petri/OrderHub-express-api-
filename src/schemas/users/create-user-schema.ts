693e026624de444ea9802e29650d0884f11a2c1d
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-05 18:45:30 -0300

    perf: improve create user shema

--- src/schemas/users/create-user-schema.ts
+++ src/schemas/users/create-user-schema.ts
@@ -2,11 +2,22 @@ import z from 'zod'
 import { stringSchema } from '../type-schemas/string-schemas'
 import { emailSchema } from '../specific-types-schemas/email-schema'
 import { passwordSchema } from '../specific-types-schemas/password-schema'
+import { userRole } from '@/utils/validation-errors'
 
 const createUserSchema = z.object({
     name: stringSchema('name', 'string'),
     email: emailSchema('email'),
     password: passwordSchema(),
+    role: z
+        .string()
+        .optional()
+        .refine(
+            (value) =>
+                value === undefined || value === 'sale' || value === 'customer',
+            {
+                message: userRole,
+            },
+        ),
 })
 
 export { createUserSchema }
