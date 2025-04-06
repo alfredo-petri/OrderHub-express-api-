510bd95d47889f15e4bca4bf6dad1c88ba0f84ea
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-06 10:33:03 -0300

    feat: create method for deliveries controller

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,9 +1,17 @@
+import { Delivery } from '@/class/delivery'
+import { createDeliverySchema } from '@/schemas/deliveries/create-delivery-schema'
 import { Request, Response, NextFunction } from 'express'
 
 class DeliveriesController {
     async create(request: Request, response: Response, next: NextFunction) {
         try {
-            return response.json({ message: 'ok' })
+            const { user_id: userId, description } = createDeliverySchema.parse(
+                request.body,
+            )
+
+            await Delivery.new({ userId, description })
+
+            return response.status(201).json()
         } catch (error) {
             next(error)
         }
