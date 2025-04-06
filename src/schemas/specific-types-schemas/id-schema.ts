da269731c9b4dfa692de7cf0267b759d4074b050
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-06 10:32:29 -0300

    perf: improve id schema

--- src/schemas/specific-types-schemas/id-schema.ts
+++ src/schemas/specific-types-schemas/id-schema.ts
@@ -4,17 +4,20 @@ import {
 } from '@/utils/validation-errors'
 import z from 'zod'
 
-export const idSchema = z
-    .string({
-        invalid_type_error: invalidTypeErrorMessage('id', 'string'),
-        required_error: requiredErrorMessage('id'),
-    })
-    .uuid(invalidTypeErrorMessage('id', 'uuid'))
-
-export const idQueryParam = z
-    .string({
-        invalid_type_error: invalidTypeErrorMessage('id', 'string'),
-        required_error: requiredErrorMessage('id'),
-    })
-    .uuid(invalidTypeErrorMessage('id', 'uuid'))
-    .optional()
+export const idSchema = (field: string) => {
+    return z
+        .string({
+            invalid_type_error: invalidTypeErrorMessage(field, 'string'),
+            required_error: requiredErrorMessage(field),
+        })
+        .uuid(invalidTypeErrorMessage(field, 'uuid'))
+}
+export const idQueryParam = (field: string) => {
+    return z
+        .string({
+            invalid_type_error: invalidTypeErrorMessage(field, 'string'),
+            required_error: requiredErrorMessage(field),
+        })
+        .uuid(invalidTypeErrorMessage(field, 'uuid'))
+        .optional()
+}
