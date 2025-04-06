e581269c3dcbe496dd577e02e739efab68e875dc
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-06 18:47:06 -0300

    feat: add new error message for sb queries

--- src/utils/db-queries-errors.ts
+++ src/utils/db-queries-errors.ts
@@ -5,3 +5,5 @@ export const credentialsError = 'email or password invalid'
 export const invalidToken = 'invalid token, unauthorized'
 export const requiredToken = 'token is required'
 export const unauthorized = 'unauthorized'
+export const shippedStatusNeeded =
+    'Invalid delivery status. The order must be marked as shipped before logging the shipping details.'
