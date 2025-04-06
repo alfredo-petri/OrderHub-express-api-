add0e59968b2fb2a37dd54fced713cb3337d127a
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-06 10:32:05 -0300

    perf: improve create delivery schema

--- src/schemas/deliveries/create-delivery-schema.ts
+++ src/schemas/deliveries/create-delivery-schema.ts
@@ -3,6 +3,6 @@ import { idSchema } from '../specific-types-schemas/id-schema'
 import { stringSchema } from '../type-schemas/string-schemas'
 
 export const createDeliverySchema = z.object({
-    user_id: idSchema,
+    user_id: idSchema('user_id'),
     description: stringSchema('description', 'string'),
 })
