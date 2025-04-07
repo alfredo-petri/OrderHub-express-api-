c022fbb797cb5fa4ff04eb0e7feb6b27f4112a89
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-07 09:27:21 -0300

    feat: add new errors for queries

--- src/utils/db-queries-errors.ts
+++ src/utils/db-queries-errors.ts
@@ -5,5 +5,7 @@ export const credentialsError = 'email or password invalid'
 export const invalidToken = 'invalid token, unauthorized'
 export const requiredToken = 'token is required'
 export const unauthorized = 'unauthorized'
+export const deliveredStatus = 'this order has already been delivered'
 export const shippedStatusNeeded =
-    'Invalid delivery status. The order must be marked as shipped before logging the shipping details.'
+    'invalid delivery status, the order must be marked as shipped before logging the shipping details.'
+export const userView = 'the user can only view their deliveries'
