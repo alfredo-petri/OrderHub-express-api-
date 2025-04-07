3a6ce28503d6aebc93f8a0c4b21b6928f7aea9c9
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-07 09:23:22 -0300

    feat: create delivery log method

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,9 +1,34 @@
+import { Delivery } from '@/class/delivery'
+import { Deliverylogs } from '@/class/delivery-logs'
+import { createDeliveryLogsSchema } from '@/schemas/delivery-logs/create-delivery-logs-schema'
+import { AppError } from '@/utils/AppError'
+import { shippedStatusNeeded } from '@/utils/db-queries-errors'
 import { Request, Response, NextFunction } from 'express'
 
 class DeliveryLogsController {
     async create(request: Request, response: Response, next: NextFunction) {
+        const { delivery_id: deliveryId, description } =
+            createDeliveryLogsSchema.parse(request.body)
+
         try {
-            return response.json({ message: 'ok' })
+            const delivery = new Delivery(deliveryId)
+
+            await delivery.init()
+
+            if (delivery.status === 'delivered') {
+                throw new AppError(shippedStatusNeeded, 422)
+            }
+
+            if (delivery.status !== 'shipped') {
+                throw new AppError(shippedStatusNeeded, 422)
+            }
+
+            await Deliverylogs.new({
+                deliveryId,
+                description,
+            })
+
+            return response.status(201).json()
         } catch (error) {
             next(error)
         }
