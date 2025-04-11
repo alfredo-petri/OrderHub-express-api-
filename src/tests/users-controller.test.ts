25182fbe0edfe3d1b525ea32fc9748e3265e8df7
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-11 10:14:38 -0300

    feat: remove created test user from db

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,7 +1,14 @@
 import { app } from '@/app'
+import { prisma } from '@/prisma'
 import request from 'supertest'
 
 describe('UsersController', () => {
+    let userId: string
+
+    afterAll(async () => {
+        await prisma.user.delete({ where: { id: userId } })
+    })
+
     it('shoukd create a new user succesfully', async () => {
         const response = await request(app).post('/users').send({
             name: 'test user',
@@ -12,5 +19,7 @@ describe('UsersController', () => {
         expect(response.status).toBe(201)
         expect(response.body.newUser).toHaveProperty('id')
         expect(response.body.newUser.name).toBe('test user')
+
+        userId = response.body.newUser.id
     })
 })
